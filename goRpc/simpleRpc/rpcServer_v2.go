2b2e399fb1c9ec50668b6c9a25e347007e6efc82
Aeishen <AeishenLin@gmail.com>
2020-05-14 23:24:17 +0800

    update

--- goRpc/simpleRpc/rpcServer_v2.go
+++ goRpc/simpleRpc/rpcServer_v2.go
@@ -9,18 +9,21 @@ import (
 )
 
 func main() {
-	_ = rpc.RegisterName("AArgs", new(rpcObjects.Args))  //通过名称注册对象
+	_ = rpc.RegisterName("Args", new(rpcObjects.Args))  //通过名称注册对象
 
 	listener, e := net.Listen("tcp", ":1234")  // 开启监听
 	if e != nil {
 		log.Fatal("Starting RPC-server -listen error:", e)
 	}
 
-	conn, e := listener.Accept()
-	if e != nil {
-		log.Fatal("Starting RPC-server -listen Accept:", e)
-	}
-
-	rpc.ServeConn(conn)
+	go func() {
+		for {
+			conn, e := listener.Accept()
+			if e != nil {
+				log.Fatal("Starting RPC-server -listen Accept:", e)
+			}
+			go rpc.ServeConn(conn)
+		}
+	}()
 	time.Sleep(1000e9)
 }
