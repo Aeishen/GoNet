ed95fd412f8f542ed7925a1a1ff9805bf2794dad
Aeishen <AeishenLin@gmail.com>
2020-06-02 21:00:30 +0800

    update:

--- goRpc/originalRpc/simpleRpc/rpcServer_v2.go
+++ goRpc/originalRpc/simpleRpc/rpcServer_v2.go
@@ -19,13 +19,13 @@ func main() {
 	go rpc.Accept(listener) //在侦听器上接受连接，并为每个传入连接提供请求
 
 	// 下面代码就是rpc.Accept的内部实现
-	for {
-		conn, e := listener.Accept() // 接收客户端连接请求
-		if e != nil {
-			log.Fatal("Starting RPC-server -listen Accept:", e)
-			continue
-		}
-		go rpc.ServeConn(conn)
-	}
+	//for {
+	//	conn, e := listener.Accept() // 接收客户端连接请求
+	//	if e != nil {
+	//		log.Fatal("Starting RPC-server -listen Accept:", e)
+	//		continue
+	//	}
+	//	go rpc.ServeConn(conn)
+	//}
 	time.Sleep(1000e9)
 }
